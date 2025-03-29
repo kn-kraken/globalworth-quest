2cddc96bd2994b3d5da77c73fb3c68f55b4029e7
julia <julia.jodczyk@gmail.com>
2025-03-30 00:17:06 +0100

    implement achievements

--- src/routes/achievements/+page.svelte
+++ src/routes/achievements/+page.svelte
@@ -1,1 +1,18 @@
-<h1>See if this works</h1>
+<script>
+    import Achievement from "$lib/components/Achievement.svelte";
+    import Overlay from "$lib/components/Overlay.svelte";
+
+    let achievement = $state([
+        { title: "Welcome Aboard!", description: "complete your basic training", rp: 55, xp: 55, status: 1 },
+        { title: "Now we're talking", description: "find the arcade room", rp: null, xp: null, status: null },
+        { title: "Party maker!", description: "host 5 events", rp: 55, xp: 55, status: 0.6 },
+    ])
+</script>
+
+<h1>All Achievements</h1>
+
+<Overlay>
+    {#each achievement as achievement}
+        <Achievement {...achievement}/>
+    {/each}
+</Overlay>
