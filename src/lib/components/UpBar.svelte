55a6dd97f1746b1bd840731e6c0deb3d52998d99
ToryLejns <47449@kozminski.edu.pl>
2025-03-30 01:22:21 +0100

    upbar allah akbar vol.3

---
+++ src/lib/components/UpBar.svelte
@@ -0,0 +1,14 @@
+<script>
+	import UpBarButton from '$lib/components/UpBarButton.svelte';
+	export let upperText
+	export let mainText
+
+</script>
+
+<div class="w-full px-4 py-2 flex items-center justify-between">
+	<div class="flex flex-col">
+		<span class="text-s text-gray-500">{upperText}</span>
+		<span class="text-3xl font-medium">{mainText}</span>
+	</div>
+	<UpBarButton/>
+</div>
